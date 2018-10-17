ff7833f549ec7af083d1cc779c3c8f5a9a3d5129
Daniel Trotter <flare84@gmail.com>
2018-10-17 13:51:35 -0600

    move state to store

--- client/src/components/ScoreCard.js
+++ client/src/components/ScoreCard.js
@@ -1,1 +1,11 @@
-export default () => null
+import React from 'react'
+import ScoreCard from './ScoreSection'
+
+const ScoreCard = () => (
+  <>
+    <ScoreSection label="Upper" />
+    <ScoreSection label="Lower" />
+  </>
+)
+
+export default ScoreCard
--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import 'semantic-ui-css/semantic.min.css';
 import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import store from './store';
+import store from './store'
 
 ReactDOM.render(
   <Provider store={store}>
